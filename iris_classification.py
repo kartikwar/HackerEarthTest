1bc4b01faaa898a7e5141d51f8e727d347500728
kartikwar <kartiksirwani@gmail.com>
2017-12-13 15:52:57 +0530

    iris classification python file added

---
+++ iris_classification.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+
+if __name__ == '__main__':
+	train = pd.read_csv('Iris.csv')
