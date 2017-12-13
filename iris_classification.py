20a6f4dad5617dcd49e3138b9fd556a49bc1ea05
kartikwar <kartiksirwani@gmail.com>
2017-12-13 16:29:05 +0530

    data visualization done

--- iris_classification.py
+++ iris_classification.py
@@ -6,6 +6,26 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
+def map_varities(species, mapping_dict):
+	species = mapping_dict[species]
+	return species
+
+def visualize_data(train):
+	print train.describe(include='all')
+	sns.barplot(x="SepalLengthCm", y="Species", data=train)
+	#graph shows sepallength is a good parameter
+	sns.barplot(x="SepalWidthCm", y="Species", data=train)
+	# graph shows sepalwidth is a good parameter
+	sns.barplot(x="PetalLengthCm", y="Species", data=train)
+	# graph shows petallength is a good parameter
+	sns.barplot(x="PetalWidthCm", y="Species", data=train)
+	# graph shows petalwidth is a good parameter
+	plt.show()
 
 if __name__ == '__main__':
 	train = pd.read_csv('Iris.csv')
+	visualize_data(train)
+	Iris_varieties_mapping = {'Iris-setosa' : 0  , 'Iris-versicolor' : 1 , 
+	'Iris-virginica' : 2}
+	train["Species"] = train["Species"].apply(map_varities, 
+	args = (Iris_varieties_mapping,))
